39879dbe91e96722b38c3e661386b3b6f369c3d4
FDDCDV <154063319+FDDCDV@users.noreply.github.com>
2025-06-28 09:53:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,6 +40,3 @@ def auto_control():
 def check_moisture():
     result = check_soil_and_notify()
     return result
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
